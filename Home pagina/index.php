730dff7c03121302640f0445d8e19286ea8ede79
Dylanomz <527457@vistacollege.nl>
2025-06-10 13:45:36 +0200

    fff

--- Home pagina/index.php
+++ Home pagina/index.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Clean Navbar</title>
+
+  <link rel="stylesheet" href="style.css">
 </head>
-<body> 
-    <div class="navbar">
-  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a>
-  <a href="#"><i class="fa fa-fw fa-search"></i> Search</a>
-  <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-  <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
-</div>
-    
+<body>
+
+<nav>
+  <div class="navbar">
+    <div class="logo">Logo</div>
+    <div class="nav-links" id="navLinks">
+      <a href="#">Home</a>
+      <a href="#">Over</a>
+      <a href="#">Diensten</a>
+      <a href="#">Contact</a>
+    </div>
+    <div class="hamburger" id="hamburger">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</nav>
 </body>
 </html>
