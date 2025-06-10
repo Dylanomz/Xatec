6a63c65a6e1f83fdf41907e53cdbbbda8b9764ac
Dylanomz <527457@vistacollege.nl>
2025-06-10 14:10:36 +0200

    wdwdadeqsds

---
+++ Cars pagina/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Clean Navbar</title>
+
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav>
+  <div class="navbar">
+    <div class="logo">Logo</div>
+    <div class="nav-links" id="navLinks">
+      <a href="#">Home</a>
+      <a href="#">Over</a>
+      <a href="#">Diensten</a>
+      <a href="#">Contact</a>
+    </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
