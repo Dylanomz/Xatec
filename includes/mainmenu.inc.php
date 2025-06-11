e50fe2cf3092be390a29fbf54a9e671da366affd
EspenVista <522581@vistacollege.nl>
2025-06-11 11:50:24 +0200

    update

---
+++ includes/mainmenu.inc.php
@@ -0,0 +1,7 @@
+<div class="nav-links" id="navLinks">
+    <a href="index.php">Home</a>
+    <a href="projects.php">Projects</a>
+    <a href="services.php">Services</a>
+    <a href="shop.php">Shop</a>
+    <a href="menu.php">Menu</a>
+</div>
---
+++ services.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Clean Navbar</title>
+
+  <link rel="stylesheet" href="furniture/style.css">
+</head>
+<body>
+
+<nav>
+  <div class="navbar">
+      <img src="img/xatec_clear.png" alt="logo" class="logo">
+    <?php require_once("includes/mainmenu.inc.php"); ?>
+    
+  </div>
+</nav>
+
+
+</body>
+</html>
