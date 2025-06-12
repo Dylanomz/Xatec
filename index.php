fbd94f300db4970b973192115b5b0eee4b565f01
EspenVista <522581@vistacollege.nl>
2025-06-12 10:19:51 +0200

    edf

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Clean Navbar</title>
+  <title>Home</title>
 
   <link rel="stylesheet" href="furniture/style.css">
   <link rel="stylesheet" href="furniture/index.css">
