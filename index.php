1fc51146da0a1469cf1ca6e44361519c529c02e6
EspenVista <522581@vistacollege.nl>
2025-06-12 14:10:38 +0200

    update

--- index.php
+++ index.php
@@ -18,41 +18,37 @@
   </div>
 </nav>
 
-<div class="slideshow-container">
+<div class="page-wrapper">
+
+  <div class="slideshow-container">
 
 
-  <div class="mySlides fade">
-    <div class="numbertext">1 / 3</div>
-    <img src="img/ferrari 559 1920-1080.jpg" style="width:100%">
-    <div class="text"><H2>Ferrari 559</H2></div>
-  </div>
+    <div class="mySlides fade">
+      <div class="numbertext">1 / 3</div>
+      <img src="img/ferrari 559 1920-1080.jpg" style="width:120%">
+      <div class="text"><H2>Ferrari 559</H2></div>
+    </div>
 
-  <div class="mySlides fade">
-    <div class="numbertext">2 / 3</div>
-    <img src="img/lambo groen 1920-1080.jpg" style="width:100%">
-    <div class="text"><h2>Lamborgini</h2></div>
-  </div>
+    <div class="mySlides fade">
+      <div class="numbertext">2 / 3</div>
+      <img src="img/lambo groen 1920-1080.jpg" style="width:120%">
+      <div class="text"><h2>Lamborgini</h2></div>
+    </div>
 
-  <div class="mySlides fade">
-    <div class="numbertext">3 / 3</div>
-    <img src="img/bugatti 1920-1080.jpg" style="width:100%">
-    <div class="text"><H2>Bugatti</H2></div>
-  </div>
+    <div class="mySlides fade">
+      <div class="numbertext">3 / 3</div>
+      <img src="img/bugatti 1920-1080.jpg" style="width:120%">
+      <div class="text"><H2>Bugatti</H2></div>
+    </div>
 
   
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
 </div>
-<br>
+  <br>
 
 <div class="slideshow-container">
-
-    <div style="text-align:center">
-      <span class="dot" onclick="currentSlide(1)"></span>
-      <span class="dot" onclick="currentSlide(2)"></span>
-      <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
-</div>
 
 <script src="furniture/index.js"></script>
 </body>
