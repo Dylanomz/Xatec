46518ce39fbc9cf82688bd66eb8b0563e8bc7775
EspenVista <522581@vistacollege.nl>
2025-06-18 11:06:07 +0200

    ycj

--- Checkout.php
+++ Checkout.php
@@ -9,6 +9,16 @@
   </head>
 <body>
 
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
 
 <div class="row">
   <div class="col-75">
@@ -40,13 +50,7 @@
           </div>
 
           <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
+           
             </div>
             <label for="cname">Name on Card</label>
             <input type="text" id="cname" name="cardname" placeholder="John More Doe">
@@ -69,7 +73,7 @@
 
         </div>
         <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+          <input type="checkbox" checked="checked" name="sameadr" required> Shipping address same as billing
         </label>
         <input type="submit" value="Continue to checkout" class="btn">
       </form>
@@ -77,6 +81,15 @@
   </div>
   </div>
 </div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
 <footer class="footer">
   <div class="footer-container">
