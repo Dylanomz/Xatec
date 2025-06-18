9f7dd0380c1f4b525d9d95cd2a5e692a21728075
EspenVista <522581@vistacollege.nl>
2025-06-18 09:38:52 +0200

    dsfsdf

--- Checkout.php
+++ Checkout.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="furniture/style.css">
     <link rel="stylesheet" href="furniture/checkout.css">
-</head>
+  </head>
 <body>
 
 <nav>
