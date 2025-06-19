1e34ca4884c4ea5d90d2741fb9dfc7573254a668
Dylanomz <527457@vistacollege.nl>
2025-06-19 11:38:59 +0200

    fesff

--- projects.php
+++ projects.php
@@ -56,11 +56,11 @@
 
       <div class="video-container">
         <video muted loop preload="metadata">
-          <source src="Videos/VolkswagenCaddy.mp4" type="video/mp4" />
+          <source src="Videos/bmw.mp4" type="video/mp4" />
           Je browser ondersteunt geen video.
         </video>
         <div class="overlay"></div>
-        <div class="title">Volkswagen T-roc R</div>
+        <div class="title">BMW F30</div>
       </div>
 
       <div class="video-container">
